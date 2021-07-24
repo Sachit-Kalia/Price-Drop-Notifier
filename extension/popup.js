90fbbfc8b0e9f488d83a5f5f88f547282f22290c
Sachit-Kalia <sachit@iitg.ac.in>
2021-07-24 13:27:14 +0530

    Task 6 completed

--- extension/popup.js
+++ extension/popup.js
@@ -1,9 +1,21 @@
 let amazonextension = angular.module("amazonextension", ["ui.router"]);
 
 amazonextension.config(function ($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise("/home");
-  $stateProvider.state("home", {
-    url: "/home",
-    templateUrl: "home.html",
-  });
+
+      $urlRouterProvider.otherwise("/login");
+
+      $stateProvider
+      .state("home", {
+        url: "/home",
+        templateUrl: "./views/home.html",
+      })
+      .state("login", {
+        url: "/login",
+        templateUrl: "./views/login.html",
+      })
+      .state("signup", {
+        url: "/signup",
+        templateUrl: "./views/signup.html",
+      });
+
 });
